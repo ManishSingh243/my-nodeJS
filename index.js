5c71b6f9263c0ef301c112ea6a83b7cb8b52e6f3
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-06-18 00:58:18 +0530

    index file

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+
+       if(url==='/'){
+        res.write('<html><body><form action="/message" method="POST">')
+        res.write('<input type="text" name="input" />')
+        res.write('<button type="submit">Enter</button>')
+        res.write("</form></body></html>")
+        return res.end()
+    }
+
+      if(url === '/message' && method === 'POST'){
+         const arr = []
+         req.on('data', (chunk)=>{
+            arr.push(chunk)
+         })
+         req.on('end', ()=>{
+            const parsedBody = Buffer.concat(arr).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, (err)=>{
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
+         })
+      }
+})
+
+server.listen(4000)
