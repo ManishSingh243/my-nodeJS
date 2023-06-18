47386c02ddb1bf771fec0757071187087e6f25c0
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-06-18 11:27:56 +0530

    route file

---
+++ routes.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+const routes = (req, res) => {
+    const url = req.url
+    const method = req.method
+
+       if(url==='/'){
+        res.write('<html><body><form action="/message" method="POST">')
+        res.write('<input type="text" name="input" />')
+        res.write('<button type="submit">Enter</button>')
+        res.write("</form></body></html>")
+        return res.end()
+    }
+
+      if(url === '/message' && method === 'POST'){
+         const arr = []
+         req.on('data', (chunk)=>{
+            arr.push(chunk)
+         })
+         req.on('end', ()=>{
+            const parsedBody = Buffer.concat(arr).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, (err)=>{
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
+         })
+      }
+    }
+
+    module.exports = routes
